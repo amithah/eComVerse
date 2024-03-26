4a7525d94d30109bd6eddeeb98eb12a5775f4518
amithah <amithah.nithin@gmail.com>
2024-03-26 18:08:09 +0400

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require("../controller/authController");
+const authController = require("../controller/AuthController");
 
 // const authMiddleware = require("../middleware/auth");
 
